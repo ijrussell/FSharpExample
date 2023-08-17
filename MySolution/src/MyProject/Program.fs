b319b652deff2e4c6b284db33782519f8b212c6e
ijrussell <ijrussell@fastmail.co.uk>
2023-08-17 11:02:22 +0100

    Added happy path in program

--- MySolution/src/MyProject/Program.fs
+++ MySolution/src/MyProject/Program.fs
@@ -1,2 +1,27 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open App
+
+let nowProvider = fun () -> DateTime.Now
+
+let companyRepository id =
+    Some { Id = id; Name = $"Some Imaginary Company"; Classification = Gold }
+
+let customerRepository (input:Customer * CustomerCredit) = ()
+
+let creditService customer = 1000
+
+let services = {
+    NowProvider = nowProvider 
+    GetCompanyById = companyRepository
+    CreateCustomer = customerRepository
+    CreditService = creditService
+}
+
+let firstName = "Geraldine"
+let surname = "Grainger-Tuppence"
+let email = "geraldine@someimaginarycompany.co.uk" 
+let dateOfBirth = DateTime(1975, 1, 13) |> DateOnly.FromDateTime
+let companyId = 42
+
+CustomerService.addCustomer services firstName surname email dateOfBirth companyId
+|> printfn "%A"
